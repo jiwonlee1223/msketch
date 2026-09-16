40a9759d4c559021d78c48295d011ed8f67616c0
jiwonlee1223 <to55540750@gmail.com>
2026-09-16 15:22:04 +0900

    initial commit

---
+++ config/passport.js
@@ -0,0 +1,121 @@
+// var LocalStrategy = require('passport-local').Strategy
+// var User = require('../models/user');
+
+// module.exports = function(passport) {
+//     passport.serializeUser(function(user, done) {
+//         done(null, user.id);
+//     });
+//     passport.deserializeUser(function(id, done) {
+//         User.findById(id, function(err, user) {
+//             done(err, user);
+//         });
+//     });
+//     //프로그램 작성
+
+//     passport.use('signup', new LocalStrategy({
+//         usernameField : 'email',
+//         passwordField : 'password',
+//         passReqToCallback : true
+//     },
+//     async function(req, email, password, done) {
+//         User.findOne({ 'email' : email }, function(err, user) {
+//             if (err) return done(err);
+//             if (user) {
+//                 //return done(null, false);
+//                 return done(null, false, req.flash('signupMessage', 'E-mail already exists.'));
+//             } else {
+//                 var newUser = new User();
+//                 newUser.name = req.body.name;
+//                 newUser.email = email;
+//                 newUser.password = newUser.generateHash(password);
+//                 newUser.save(function(err) {
+//                     if (err)
+//                         throw err;
+//                     return done(null, newUser);
+//                 });
+//             }
+//         });
+//     }));
+
+//     passport.use('login', new LocalStrategy({
+//             usernameField : 'email',
+//             passwordField : 'password',
+//             passReqToCallback : true
+//         },
+//         async function(req, email, password, done) {
+//             User.findOne({ 'email' : email }, function(err, user) {
+//                 if (err)
+//                     return done(err);
+//                 if (!user)
+//                     //return done(null, false);
+//                     return done(null, false, req.flash('loginMessage', 'Cannot find user e-mail.'));
+//                 if (!user.validPassword(password))
+//                     //return done(null, false);
+//                     return done(null, false, req.flash('loginMessage', 'Password is not correct.'));
+
+//                 //console.log("login Ok")
+//                 return done(null, user);
+//             });
+//         }));
+
+// };
+
+var LocalStrategy = require('passport-local').Strategy;
+var User = require('../models/user');
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user.id);
+    });
+
+    passport.deserializeUser(function(id, done) {
+        User.findById(id).exec()
+            .then(user => done(null, user))
+            .catch(err => done(err));
+    });
+
+    passport.use('signup', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    async function(req, email, password, done) {
+        try {
+            let user = await User.findOne({ 'email': email }).exec();
+            if (user) {
+                return done(null, false, req.flash('signupMessage', 'E-mail already exists.'));
+            } else {
+                let newUser = new User();
+                newUser.name = req.body.name;
+                newUser.email = email;
+                newUser.password = newUser.generateHash(password);
+                
+                await newUser.save();
+                return done(null, newUser);
+            }
+        } catch (err) {
+            return done(err);
+        }
+    }));
+
+    passport.use('login', new LocalStrategy({
+        usernameField: 'email',
+        passwordField: 'password',
+        passReqToCallback: true
+    },
+    async function(req, email, password, done) {
+        try {
+            let user = await User.findOne({ 'email': email }).exec();
+            if (!user) {
+                return done(null, false, req.flash('loginMessage', 'Cannot find user e-mail.'));
+            }
+            if (!user.validPassword(password)) {
+                return done(null, false, req.flash('loginMessage', 'Password is not correct.'));
+            }
+            return done(null, user);
+        } catch (err) {
+            return done(err);
+        }
+    }));
+};
+
---
+++ config/routes.js
@@ -0,0 +1,148 @@
+var User = require('../models/user');
+
+module.exports = function(app, passport, fs) {
+
+    // Login
+    app.get('/', isLoggedIn, function(req, res){
+        console.log(req.user)
+        res.render('index', {user: req.user})
+    });
+
+    app.get('/login', function(req, res){
+        res.render('login', {message: req.flash('loginMessage')})
+    });
+
+    app.get('/signup', function(req, res){
+        res.render('signup', {message: req.flash('signupMessage')})
+    });
+
+    app.get('/logout', function(req, res){
+        req.logout();
+        res.redirect('/');
+    });
+
+    app.get('/manual', isLoggedIn, function(req, res){
+        res.render('manual', {user: req.user})
+    });
+
+    app.post('/signup', passport.authenticate('signup', {
+        successRedirect : '/login',
+        failureRedirect : '/signup', //가입 실패시 redirect할 url주소
+        failureFlash : false
+    }));
+    app.post('/login', passport.authenticate('login', {
+            //successRedirect : '/profile',
+            //failureRedirect : '/', //로그인 실패시 redirect할 url주소
+            //failureFlash : false
+            failureRedirect : '/'
+        }),
+        function(req, res){
+            res.redirect('/')
+        }
+    );
+
+    // Setting IO
+    app.post('/saveSettings', function(req, res){
+        var dir = './settings/';
+
+        fs.writeFile( dir + '/'+req.user.id+'.json', req.body["settings"], function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            res.sendStatus(200);
+        });
+    });
+    app.post('/loadSettings', function(req, res){
+        var dir = './settings/';
+        var fileName = req.user.id + '.json';
+        if (!fs.existsSync(dir+fileName)){
+            fileName = "default.json";
+        }
+        fs.readFile(dir+"/"+fileName, 'utf8', function(err, data){
+            if(err) {
+                return console.log(err);
+            }
+            res.send({settings: data});
+        });
+    });
+
+    // File IO
+    app.post('/save', function(req, res){
+        var dir = './storage/'+req.user.id;
+        if (!fs.existsSync(dir)){
+            fs.mkdirSync(dir);
+        }
+        fs.writeFile( dir + '/'+req.body["fileName"]+'.json', req.body["file"], function(err) {
+            if(err) {
+                return console.log(err);
+            }
+            res.sendStatus(200);
+        });
+    });
+    app.post('/loadFile', function(req, res){
+        var dir = './storage/'+req.user.id;
+        fs.readdir(dir, function(err, files) {
+            if(err) {
+                return console.log(err);
+            }
+            var fileList = [];
+            files.forEach(function(f) {
+                fileList.push(f);
+            });
+            res.send({list: fileList});
+        });
+    });
+
+    app.post('/openFile', function(req, res){
+        var dir = './storage/'+req.user.id;
+        var fileName = req.body["fileName"];
+        fs.readFile(dir+"/"+fileName, 'utf8', function(err, data){
+            if(err) {
+                return console.log(err);
+            }
+            res.send({file: data, name: fileName});
+        });
+    });
+
+    app.post('/uploadFile', function(req, res){
+    });
+
+    app.post('/renameFile', function(req, res){
+    });
+
+    app.post('/deleteFile', function(req, res){
+        var dir = './storage/'+req.user.id;
+        var fileName = req.body["fileName"];
+        fs.unlink(dir+"/"+fileName, function(err, data){
+            if(err) {
+                return console.log(err);
+            }
+            fs.readdir(dir, function(err, files) {
+                if(err) {
+                    return console.log(err);
+                }
+                var fileList = [];
+                files.forEach(function(f) {
+                    fileList.push(f);
+                });
+                res.send({list: fileList});
+            });
+        });
+    });
+    /*
+    app.get('/find', function(req,res){
+        User.find(function(err, books){
+            if(err) return res.status(500).send({error: 'database failure'});
+            res.json(books);
+        })
+    });
+    */
+
+    function isLoggedIn(req, res, next) {
+        if (req.isAuthenticated()){
+            return next();
+        } else {
+            res.redirect('/login');
+        }
+    }
+}
